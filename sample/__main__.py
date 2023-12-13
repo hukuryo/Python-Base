a18874c324a9de67b178739280c08412ac5799f3
福島 涼平 <ryohei.1998.05.09@gmail.com>
2023-12-13 16:29:43 +0900

    推奨されたディレクトリ構成にしてみた

---
+++ sample/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import fib
+
+def main():
+    fib.sample_func()
+    fib.sample_func2()
+
+
+if __name__ == '__main__':
+    main()
---
+++ sample/fib.py
@@ -0,0 +1,5 @@
+def sample_func():
+    print('sample_funcの呼び出し!')
+    
+def sample_func2():
+    print('sample_func2の呼び出し')
