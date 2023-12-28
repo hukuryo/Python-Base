cb0a7938471fcd3cfa50ae34f116f43935d507af
福島 涼平 <ryohei.1998.05.09@gmail.com>
2023-12-28 09:22:58 +0900

    Jsonファイルの読み書きを勉強

--- sample/file_operation.py
+++ sample/file_operation.py
@@ -1,3 +1,5 @@
+import json
+
 def file_operation():
     # f = open('file.txt')
     # for line in f:
@@ -9,6 +11,25 @@ def file_operation():
     # f.close()
     
     # 'w'を付けることでファイルに書き込むことを宣言する。
-    f = open('file.txt', 'w')
-    f.write('Hello, World!\n')
-    f.close()
+    # f = open('file.txt', 'w')
+    # f.write('Hello, World!\n')
+    # f.close()
+    
+    # with open('file.txt') as f:
+    #     for line in f:
+    #         print(line)
+    
+    # Jsonファイル読み込み
+    # with open('example.json') as f:
+    #     json_deta = json.load(f)
+    #     print(json_deta)
+    
+    json_data = {
+        "message": "Hello, World!",
+        "items": [0, 1, 2],
+        "ok": True,
+    }
+    
+    with open("example2.json", "w") as f:
+        json.dump(json_data, f)
+    
